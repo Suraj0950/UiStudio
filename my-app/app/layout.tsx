fc4f300d582184974495d351f3a167acc3e2f8c9
Suraj0950 <surajsuryaveer@gmail.com>
2026-01-08 16:39:39 +0530

    Initial UI Studio setup

---
+++ my-app/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const appFont = DM_Sans ({
+    subsets: ['latin']
+});
+
+export const metadata: Metadata = {
+  title: "uiStudio",
+  description: "Generate Free Ui-Ux for mobioe and wesites",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className = {appFont.className}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
